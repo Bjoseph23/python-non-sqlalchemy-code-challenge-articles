e24685f4721d100bb71f5b0d20717acfeaf220e1
Bill Mageni <+>
2024-06-10 07:34:23 +0300

    Main deliverables have been met

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,84 @@
 class Article:
+    all=[]
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self._title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if not isinstance(title, str) or len(title) < 5 or len(title) > 50:
+            raise ValueError("Article title must be a string between 5 and 50 characters.")
+        self._title=title
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) <= 0:
+            raise ValueError("Author name must be a non-empty string.")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_names):
+        self.new_names = new_names
+        return self._name
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self ]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles()]))
 
     def add_article(self, magazine, title):
-        pass
+        articles= Article(self, magazine, title)
+        return articles
 
     def topic_areas(self):
         pass
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if not isinstance(name, str) or len(name) < 2 or len(name) > 16:
+            raise ValueError("Magazine name must be a string between 2 and 16 characters.")
+        self._name = name
+
+        if not isinstance(category, str) or len(category) <= 0:
+            raise ValueError("Magazine category must be a non-empty string.")
+        self._category = category
+
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def category(self):
+        return self._category
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @category.setter
+    def category(self, category):
+        self._category = category
 
     def articles(self):
-        pass
+        return [articles for articles in Article.all if articles.magazine == self]
 
     def contributors(self):
-        pass
+        return [ articles.author for articles in self.articles()]
 
     def article_titles(self):
         pass
 
     def contributing_authors(self):
         pass
+    
--- lib/debug.py
+++ lib/debug.py
@@ -11,3 +11,72 @@ if __name__ == '__main__':
 
     # don't remove this line, it's for debugging!
     ipdb.set_trace()
+
+class Author:
+    def __init__(self, name):
+        if not isinstance(name, str) or len(name) <= 0:
+            raise ValueError("Author name must be a non-empty string.")
+        self._name = name
+        self._name.articles=[]
+
+        @property
+        def name(self):
+            return self._name
+        
+        def articles(self):
+            return [article for article in Article if article.author == self ]
+        
+        def magazines(self):
+            return [article.magazine for article in self.articles()]
+        
+        def add_articles(magazine, title):
+            pass
+        
+
+class Magazine:
+    def __init__(self, name, category):
+        if not isinstance(name, str) or len(name) < 2 or len(name) > 16:
+            raise ValueError("Magazine name must be a string between 2 and 16 characters.")
+        self._name = name
+
+        if not isinstance(category, str) or len(category) <= 0:
+            raise ValueError("Magazine category must be a non-empty string.")
+        self._category = category
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str) or len(name) < 2 or len(name) > 16:
+            raise ValueError("Magazine name must be a string between 2 and 16 characters.")
+        self._name = name
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, category):
+        if not isinstance(category, str) or len(category) <= 0:
+            raise ValueError("Magazine category must be a non-empty string.")
+        self._category = category
+
+class Article:
+    def __init__(self, author, magazine, title):
+        if not isinstance(author, Author):
+            raise TypeError("Author must be an Author instance.")
+        self._author = author
+
+        if not isinstance(magazine, Magazine):
+            raise TypeError("Magazine must be a Magazine instance.")
+        self._magazine = magazine
+
+        if not isinstance(title, str) or len(title) < 5 or len(title) > 50:
+            raise ValueError("Article title must be a string between 5 and 50 characters.")
+        self._title = title
+
+    @property
+    def title(self):
+        return self._title
